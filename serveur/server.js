96a9b1fde4d7065f87083f7ba69120c804ac2801
EliasMoussamih <128966201+EliasMoussamih@users.noreply.github.com>
2025-10-07 14:15:21 +0200

    Update server.js

--- serveur/server.js
+++ serveur/server.js
@@ -1,32 +1,39 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-
 import express from "express";
 import cors from "cors";
 import connecterBD from "./src/config/bd.js";
 import routeurAuthentification from "./src/routes/authentification.js";
 import routeurContacts from "./src/routes/contacts.js";
 
-
 import swaggerUi from "swagger-ui-express";
 import swaggerJsdoc from "swagger-jsdoc";
 
-
+// Affiche l'URL MongoDB pour debug
 console.log("MONGODB_URL =", process.env.MONGODB_URL);
+
+// Connexion à MongoDB
 connecterBD();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+// Configuration CORS
+const corsOptions = {
+  origin: [
+    "http://localhost:3000", // dev
+    "https://stellular-valkyrie-753a69.netlify.app", // front Netlify
+  ],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+// Middleware pour lire le body JSON et URL-encoded
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.use(express.json());
-
-
+// Route racine
 app.get("/", (req, res) => {
   res.send("Serveur MyContacts en ligne !");
 });
@@ -35,7 +42,7 @@ app.get("/", (req, res) => {
 app.use("/authentification", routeurAuthentification);
 app.use("/contacts", routeurContacts);
 
-// Configuration de Swagger
+// Swagger
 const options = {
   definition: {
     openapi: "3.0.0",
@@ -60,7 +67,7 @@ const options = {
 const specs = swaggerJsdoc(options);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
-
+// Lancer le serveur
 app.listen(PORT, () => {
   console.log(`Le serveur tourne sur : http://localhost:${PORT}`);
 });
